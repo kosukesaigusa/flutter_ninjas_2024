1c259dab52da5288c315d13f653dd757b7b9cfb6
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-06-07 17:22:32 +0900

    feat:  remove unnecessary codes

--- packages/client/lib/auth.dart
+++ packages/client/lib/auth.dart
@@ -1,8 +1,4 @@
-import 'dart:convert';
-
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter_line_sdk/flutter_line_sdk.dart';
-import 'package:http/http.dart' as http;
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth.g.dart';
@@ -33,31 +29,10 @@ class Auth extends _$Auth {
     });
   }
 
-  Future<void> signInWithLine() async {
-    state = const AsyncLoading<User?>().copyWithPrevious(state);
-    state = await AsyncValue.guard(() async {
-      final loginResult = await LineSDK.instance.login();
-      final accessToken =
-          loginResult.accessToken.data['access_token'] as String;
-      final uri = Uri.parse(
-        'https://createfirebaseauthcustomtoken-cwi73pbowq-an.a.run.app',
-      );
-      final response =
-          await http.post(uri, body: jsonEncode({'accessToken': accessToken}));
-      final customToken = (jsonDecode(response.body)
-          as Map<String, dynamic>)['customToken'] as String;
-
-      final userCredential =
-          await FirebaseAuth.instance.signInWithCustomToken(customToken);
-      return userCredential.user;
-    });
-  }
-
   Future<void> signOut() async {
     state = const AsyncLoading<User?>().copyWithPrevious(state);
     state = await AsyncValue.guard(() async {
       await FirebaseAuth.instance.signOut();
-      await LineSDK.instance.logout();
       return null;
     });
   }
--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -1,27 +1,21 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_line_sdk/flutter_line_sdk.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'firebase_options.dart';
-import 'submission_list.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  if (!kIsWeb) {
-    await LineSDK.instance.setup('2003353308');
-  }
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(
     ProviderScope(
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: const SubmissionListPage(),
+        home: const App(),
         scrollBehavior: const MaterialScrollBehavior().copyWith(
           dragDevices: {
             PointerDeviceKind.mouse,
@@ -38,4 +32,20 @@ void main() async {
 Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   await Firebase.initializeApp();
   print('🔔 Handling a background message: ${message.messageId}');
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('FlutterNinjas 2024'),
+      ),
+      body: const Center(
+        child: Text('Hello, FlutterFire Ninjas!'),
+      ),
+    );
+  }
 }
