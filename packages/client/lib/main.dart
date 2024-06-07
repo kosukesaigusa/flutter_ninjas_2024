798cc56d4bd7905bc7a8d136de289d3215d0cd55
Kosuke Saigusa <saigusa758cloudy@gmail.com>
2024-06-07 23:00:32 +0900

    feat:  update client app example

--- packages/client/lib/main.dart
+++ packages/client/lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'firebase_options.dart';
 
@@ -12,18 +13,16 @@ void main() async {
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
 
   runApp(
-    ProviderScope(
-      child: MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: const App(),
-        scrollBehavior: const MaterialScrollBehavior().copyWith(
-          dragDevices: {
-            PointerDeviceKind.mouse,
-            PointerDeviceKind.touch,
-            PointerDeviceKind.stylus,
-            PointerDeviceKind.unknown,
-          },
-        ),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const App(),
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.unknown,
+        },
       ),
     ),
   );
@@ -34,17 +33,69 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   print('🔔 Handling a background message: ${message.messageId}');
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    () async {
+      await FirebaseAuth.instance.signInAnonymously();
+      final messaging = FirebaseMessaging.instance;
+      final settings = await messaging.requestPermission();
+      print('User granted permission: ${settings.authorizationStatus}');
+      FirebaseMessaging.onMessage.listen((message) {
+        print('Got a message whilst in the foreground!');
+        print('Message data: ${message.data}');
+        if (message.notification != null) {
+          print(
+            'Message also contained a notification: ${message.notification}',
+          );
+        }
+      });
+    }();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('FlutterNinjas 2024'),
-      ),
-      body: const Center(
-        child: Text('Hello, FlutterFire Ninjas!'),
+      appBar: AppBar(title: const Text('FlutterNinjas 2024')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 96,
+              width: 96,
+              child: Image(image: AssetImage('assets/flutter_ninjas.png')),
+            ),
+            const SizedBox(height: 32),
+            ElevatedButton(
+              onPressed: () async {
+                final apnsToken =
+                    await FirebaseMessaging.instance.getAPNSToken();
+                if (apnsToken == null) {
+                  return;
+                }
+                final token = await FirebaseMessaging.instance.getToken();
+                final uid = FirebaseAuth.instance.currentUser?.uid;
+                if (token == null || uid == null) {
+                  return;
+                }
+                await FirebaseFirestore.instance
+                    .collection('participants')
+                    .doc(uid)
+                    .set({'token': token});
+              },
+              child: const Text('Join FlutterNinjas!'),
+            ),
+          ],
+        ),
       ),
     );
   }
